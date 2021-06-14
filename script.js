f56ed5d36b8c30402c05b7dd4553683c96d47472
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-14 22:19:51 +0530

    Numeric Operators

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
 // JavaScript - Operators - BODMAS Rule
+// BODMAS: Brackets, Orders, Division/Multiplication, Addition/Subtraction.
 
-const value = 5 + 2 * 3;
-console.log(value);
+// const value = 5 + 2 * 3;
+// const value = Math.pow(2,4);
+let num = 2;
+// num += 30;
+// num -= 30;
+// num *= 2;
+// num /= 2;
+// num %= 4;
+num **= 4;
+
+console.log(num);
