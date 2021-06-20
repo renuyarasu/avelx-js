712260eb63b4b87a966497855c8f2b2f951057b8
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-20 18:02:09 +0530

    Arrays: splice()

--- script.js
+++ script.js
@@ -11,9 +11,10 @@ var arr = [
   }
 ]
 
-// Array.prototype.push()
-// The push() method adds one or more elements to the end of an array and returns the new length of the array.
+// Array.prototype.splice()
+// The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. To access part of an array without modifying it, see slice().
 
-arr.push('string', 20, 20.02, function () { }, {}, [])
+arr.splice(2, 0, 'string', 20, 20.02, function () { }, {}, []); // from 0 index
+arr.splice(2, 3, 'string', 20, 20.02, function () { }, {}, []); // from 3 index
 console.log(arr);
 console.log(arr.length);
