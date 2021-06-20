66e3409199a55a7b8187c8f7c8ea7e043fe4c18b
Renuka Prasad Yarasu <renukaprasad.yarasu@innroad.com>
2021-06-20 21:23:41 +0530

    JavaScript: Scope

--- script.js
+++ script.js
@@ -1,20 +1,14 @@
-// JavaScript - Embeding Objects And Arrays 
+// JavaScript - Functions as first class citizens
 console.clear();
 
-// Array Methods
-var arr = [
-  'string',
-  ['embed', 200],
-  { car: 'Ford' },
-  function () {
-    return 'Drive';
+// JavaScript: Scope and Closures
+let engine = {
+  maker: 'Ford',
+  headGasket: {
+    pots: [
+      'Piston_01',
+      'Piston_02',
+    ]
   }
-]
-
-// Array.prototype.splice()
-// The splice() method changes the contents of an array by removing or replacing existing elements and/or adding new elements in place. To access part of an array without modifying it, see slice().
-
-arr.splice(2, 0, 'string', 20, 20.02, function () { }, {}, []); // from 0 index
-arr.splice(2, 3, 'string', 20, 20.02, function () { }, {}, []); // from 3 index
-console.log(arr);
-console.log(arr.length);
+}
+console.log(engine.headGasket.pots[0]);
