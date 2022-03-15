228e5542c005fdab06e564140e790aa3acf49bc1
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-15 21:49:41 +0530

    If Statement - Cross the Road Task

--- script.js
+++ script.js
@@ -2,13 +2,16 @@
 console.clear();
 
 // If Statements
-let carsleft = 0;
+let carsleft = 1;
 let carsright = 0;
-let greenMan = 'no';
+let greenMan = 'yes';
+
 if (greenMan === 'yes') {
     console.log('Cross The Road!');
-} else if (carsleft === 0) {
+}
+else if (greenMan === 'yes' && carsleft === 0 && carsright === 0) {
     console.log('All clear, Cross the road!');
-} else {
+}
+else {
     console.log('Stay there!');
 }
