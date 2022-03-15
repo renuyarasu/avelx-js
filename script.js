9da343d263d7c2e318b736d297ed7f87e6639812
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-15 20:47:04 +0530

    Constructor Functions - multi instance

--- script.js
+++ script.js
@@ -9,5 +9,11 @@ function Apple(x, y, color, score) {
     this.color = color;
     this.score = score;
 }
-let result = new Apple(10, 100, 'Red', 1000);
-console.log(result); // Apple { x: 10, y: 100, color: 'Red', score: 1000 }
+let apple_01 = new Apple(10, 100, 'Red', 1000);
+let apple_02 = new Apple(10, 100, 'Green', 1000);
+let apple_03 = new Apple(10, 100, 'Pink', 1000);
+
+
+console.log(apple_01); // Apple { x: 10, y: 100, color: 'Red', score: 1000 }
+console.log(apple_02); // Apple { x: 10, y: 100, color: 'Green', score: 1000 }
+console.log(apple_03); // Apple { x: 10, y: 100, color: 'Pink', score: 1000 }
