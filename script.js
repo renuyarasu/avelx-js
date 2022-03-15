c9da64ca3c9aa3a034385c7c2c487345d73f93d5
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-15 20:54:06 +0530

    Constructor Functions - multi instance - Cycle task

--- script.js
+++ script.js
@@ -2,18 +2,21 @@
 console.log(this);
 
 // Constructor Functions
-
-function Apple(x, y, color, score) {
-    this.x = x;
-    this.y = y;
+// The constructor method is a special method of a class for creating and initializing an object instance of that class.
+function Cycle(color, price, model, brand) {
     this.color = color;
-    this.score = score;
+    this.price = price;
+    this.model = model;
+    this.brand = brand;
 }
-let apple_01 = new Apple(10, 100, 'Red', 1000);
-let apple_02 = new Apple(10, 100, 'Green', 1000);
-let apple_03 = new Apple(10, 100, 'Pink', 1000);
+let cycle_01 = new Cycle('Red', 5000, 'M_01', 'Hero');
+let cycle_02 = new Cycle('Blue', 10000, 'M_02', 'Atlas');
+let cycle_03 = new Cycle('Red', 15000, 'M_01', 'Montra');
+let cycle_04 = new Cycle('Red', 20000, 'M_01', 'Hercules');
+let cycle_05 = new Cycle('Red', 25000, 'M_01', 'Firefox');
 
-
-console.log(apple_01); // Apple { x: 10, y: 100, color: 'Red', score: 1000 }
-console.log(apple_02); // Apple { x: 10, y: 100, color: 'Green', score: 1000 }
-console.log(apple_03); // Apple { x: 10, y: 100, color: 'Pink', score: 1000 }
+console.log(cycle_01);
+console.log(cycle_02);
+console.log(cycle_03);
+console.log(cycle_04);
+console.log(cycle_05);
