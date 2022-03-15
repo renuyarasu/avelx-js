9bacb314a9328696c6d0f230bf886403a53aeca5
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-15 21:35:24 +0530

    If Statement

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 // JavaScript | this keyword | 3.31.31
 console.clear();
 
-// Constructor - Prototypal inheritance
-
-function Apple(color, weight) {
-    this.color = color;
-    this.weight = weight;
-}
-Apple.prototype = {
-    eat: () => 'Eat the Apple',
-    throw: () => 'Throw the Apple',
-}
-Apple.prototype.eat = () => 'Not eat the Apple'
-let apple_01 = new Apple('Red', 33);
-let apple_02 = new Apple('Green', 66);
-let apple_03 = new Apple('Yellow', 99);
-
-console.log(apple_01.eat());
+// If Statements
+let carsleft = 0;
+let carsright = 0;
+let greenMan = 'yes';
+if (greenMan === 'yes') {
+    console.log('Cross The Road!');
+} 
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,1 @@
+greenMan === 'yes'
