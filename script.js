03c11b568923821354f4f4c8d0807e18bca11aeb
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-16 20:48:14 +0530

    DOM: append insertBefore div Element

--- script.js
+++ script.js
@@ -7,4 +7,6 @@ let element = document.createElement('div');
 element.style.cssText = 'width:200px;height:50px;background:green';
 element.onclick = ()=> alert('INDIA');
 
-document.body.appendChild(element)
+// document.body.appendChild(element);
+let target = document.getElementById('goldenrod');
+document.body.insertBefore(element, target)
