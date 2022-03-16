dab300c46f949a0397780377cc65e914153ea6cb
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-16 21:27:52 +0530

    Final Project

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 // JavaScript | this keyword | 3.31.31
 console.clear();
 
-// Create & Append, InsertBefore & InsertAfter
+// Final Ppoject
+let elements = document.getElementsByName('cssProperty');
+let div = document.getElementById('midify');
 
-let element = document.createElement('div');
-element.style.cssText = 'width:200px;height:50px;background:green';
-element.onclick = ()=> alert('INDIA');
 
-// document.body.appendChild(element);
-let target = document.getElementById('goldenrod');
-document.body.insertBefore(element, target)
+function set() {
+    
+    for (let i = 0; i < elements.length; i++) {
+        let cssProperty = elements[i].getAttribute('id');
+        let cssValue = elements[i].value;
+        div.style[cssProperty] = cssValue;
+    }
+}
+document.getElementById('set').addEventListener('click', set);
