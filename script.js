6f45663f572b6ac0755811712f043b21059d21ac
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-16 16:24:19 +0530

    DOM: Nodes

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
 // JavaScript | this keyword | 3.31.31
 console.clear();
 
-// Var, Const & Let ES6
-for (let i = 0; i < 5; i++) {
-    var varName = 'Value reference in memory!';
-    let letName = 'Scoped value!';
-    const constName = 'Constant value in memory!';
-}
-console.log(varName); // Value reference in memory!
-console.log(letName); // letName is not defined
-console.log(constName); // constName is not defined
+// Understanding the Document Object Model (DOM)
+// Targeting DOM Elements
+
+let element = document.childNodes[1].childNodes[1].childNodes[2]
+
+console.log(element);
