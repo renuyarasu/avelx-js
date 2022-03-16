b2845c7e02b366c0651d2ffd7724dbe48635782d
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-16 16:25:18 +0530

    DOM: getElementsByTagName

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ console.clear();
 // Understanding the Document Object Model (DOM)
 // Targeting DOM Elements
 
-let element = document.childNodes[1].childNodes[1].childNodes[2]
+let element = document.getElementsByTagName('p');
 
 console.log(element);
