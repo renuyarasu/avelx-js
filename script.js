5cac0aa994ed589824ac1783e0ba9a847478fd05
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-16 16:34:44 +0530

    DOM: innerHTML

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ console.clear();
 // Understanding the Document Object Model (DOM)
 // Targeting DOM Elements
 
-let element = document.querySelectorAll('#content, .content');
+let element = document.getElementById('content').innerHTML;
+// 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. <span>Deserunt dolore odio voluptates repudiandae iure eos quam atque omnis quos exercitationem?</span>'
 
 console.log(element);
