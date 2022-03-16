179eba32205c0053edefd6d4725e082c2b2d319a
Renuka Prasad <renu.yarasu@gmail.com>
2022-03-16 16:27:29 +0530

    DOM: getElementsByClassName

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ console.clear();
 // Understanding the Document Object Model (DOM)
 // Targeting DOM Elements
 
-let element = document.getElementById('content');
+let element = document.getElementsByClassName('content');
 
 console.log(element);
